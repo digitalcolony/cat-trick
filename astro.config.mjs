9eb2a5616f07360c19c6c5f345fbaefabb04e2e5
Michael Allen Smith <digitalcolony@gmail.com>
2023-11-18 06:41:05 -0800

    static test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({
-	output: "server",
-	adapter: netlify(),
-});
+// export default defineConfig({
+// 	output: "server",
+// 	adapter: netlify(),
+// });
