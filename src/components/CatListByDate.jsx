e1f2abb65e675db7b0c15d346bb64ffd02fa3c9d
Michael Allen Smith <digitalcolony@gmail.com>
2024-01-21 08:00:39 -0800

    scroll branch merged

---
+++ src/components/CatListByDate.jsx
@@ -0,0 +1,138 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import abbey from "../assets/abbey.jpg";
+import akima from "../assets/akima.jpg";
+import boo from "../assets/boo.jpg";
+import buddy from "../assets/buddy.jpg";
+import captainjack from "../assets/captain-jack.jpg";
+import checkers from "../assets/checkers.jpg";
+import chucky from "../assets/chucky.jpg";
+import frankthetank from "../assets/frank-the-tank.jpg";
+import gerald from "../assets/gerald.jpg";
+import roket from "../assets/roket.jpg";
+import royal from "../assets/royal.jpg";
+import scarlett from "../assets/scarlett.jpg";
+import tiger from "../assets/tiger.jpg";
+import thunder from "../assets/thunder.jpg";
+import nophoto from "../assets/nophoto.jpg";
+
+export default function CatListByDate({ catsByDate }) {
+	const [visibleDays, setVisibleDays] = useState(3);
+	const lastCatElementRef = useRef(null);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			if (entries[0].isIntersecting) {
+				setVisibleDays((prevVisibleDays) => prevVisibleDays + 1);
+			}
+		});
+
+		if (lastCatElementRef.current) {
+			observer.observe(lastCatElementRef.current);
+		}
+
+		return () => {
+			if (lastCatElementRef.current) {
+				observer.unobserve(lastCatElementRef.current);
+			}
+		};
+	}, [visibleDays]);
+
+	function formatDate(dateStr) {
+		const dateObj = new Date(dateStr);
+		const dayOfWeek = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(dateObj);
+		const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(dateObj);
+		const day = dateObj.getDate();
+		const year = dateObj.getFullYear();
+		return `${dayOfWeek} ${month} ${day}, ${year}`;
+	}
+
+	function getCatImage(cat) {
+		let catImport = cat.replace(/\s+/g, "").toLowerCase();
+
+		switch (catImport) {
+			case "abbey":
+				catImport = abbey;
+				break;
+			case "akima":
+				catImport = akima;
+				break;
+			case "boo":
+				catImport = boo;
+				break;
+			case "buddy":
+				catImport = buddy;
+				break;
+			case "captainjack":
+				catImport = captainjack;
+				break;
+			case "checkers":
+				catImport = checkers;
+				break;
+			case "chucky":
+				catImport = chucky;
+				break;
+			case "frankthetank":
+				catImport = frankthetank;
+				break;
+			case "gerald":
+				catImport = gerald;
+				break;
+			case "roket":
+				catImport = roket;
+				break;
+			case "royal":
+				catImport = royal;
+				break;
+			case "scarlett":
+				catImport = scarlett;
+				break;
+			case "tiger":
+				catImport = tiger;
+				break;
+			case "thunder":
+				catImport = thunder;
+				break;
+			default:
+				catImport = nophoto;
+		}
+
+		return catImport;
+	}
+
+	return (
+		<div className="catlist">
+			{catsByDate.slice(0, visibleDays).map((group, index) => (
+				<div key={index}>
+					<p className="center date">
+						{group.cats.length >= 3 ? "🏆 " : ""}
+						{formatDate(group.date)}
+						{group.cats.length >= 3 ? " 🏆" : ""}
+					</p>
+
+					<div className="image-row">
+						{group.cats.map((cat, catIndex) => {
+							let catImport = getCatImage(cat);
+
+							if (index === visibleDays - 1 && catIndex === group.cats.length - 1) {
+								return (
+									<figure key={catIndex} ref={lastCatElementRef}>
+										<img src={catImport.src} alt={cat} />
+										<figcaption>{cat}</figcaption>
+									</figure>
+								);
+							} else {
+								return (
+									<figure key={catIndex}>
+										<img src={catImport.src} alt={cat} />
+										<figcaption>{cat}</figcaption>
+									</figure>
+								);
+							}
+						})}
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
