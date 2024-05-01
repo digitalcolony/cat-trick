5e611c64a6ea5d71e0a2cf25526bf105c67b9ba2
Michael Allen Smith <digitalcolony@gmail.com>
2024-05-01 07:56:46 -0700

    nickname

--- src/components/CatListByDate.jsx
+++ src/components/CatListByDate.jsx
@@ -54,8 +54,9 @@ export default function CatListByDate({ catsByDate }) {
 		return `${dayOfWeek} ${month} ${day}, ${year}`;
 	}
 
-	function getCatImage(cat) {
+	function getCat(cat) {
 		let catImport = cat.replace(/\s+/g, "").toLowerCase();
+		let isNickname = false;
 
 		switch (catImport) {
 			case "akima":
@@ -66,6 +67,7 @@ export default function CatListByDate({ catsByDate }) {
 				break;
 			case "boots":
 				catImport = boots;
+				isNickname = true;
 				break;
 			case "buddy":
 				catImport = buddy;
@@ -93,6 +95,7 @@ export default function CatListByDate({ catsByDate }) {
 				break;
 			case "muffin":
 				catImport = muffin;
+				isNickname = true;
 				break;
 			case "pezzetina":
 				catImport = pezzetina;
@@ -102,6 +105,7 @@ export default function CatListByDate({ catsByDate }) {
 				break;
 			case "royal":
 				catImport = royal;
+				isNickname = true;
 				break;
 			case "scarlett":
 				catImport = scarlett;
@@ -111,21 +115,25 @@ export default function CatListByDate({ catsByDate }) {
 				break;
 			case "sunny":
 				catImport = sunny;
+				isNickname = true;
 				break;
 			case "tiger":
 				catImport = tiger;
+				isNickname = true;
 				break;
 			case "thunder":
 				catImport = thunder;
+				isNickname = true;
 				break;
 			case "turtle":
 				catImport = turtle;
+				isNickname = true;
 				break;
 			default:
 				catImport = nophoto;
 		}
 
-		return catImport;
+		return { catImport, isNickname };
 	}
 
 	return (
@@ -140,20 +148,27 @@ export default function CatListByDate({ catsByDate }) {
 
 					<div className="image-row">
 						{group.cats.map((cat, catIndex) => {
-							let catImport = getCatImage(cat);
+							// get catImport and isNickname
+							const { catImport, isNickname } = getCat(cat);
 
 							if (index === visibleDays - 1 && catIndex === group.cats.length - 1) {
 								return (
 									<figure key={catIndex} ref={lastCatElementRef}>
 										<img src={catImport.src} alt={cat} />
-										<figcaption>{cat}</figcaption>
+										<figcaption style={{ fontStyle: isNickname ? "italic" : "normal" }}>
+											{isNickname ? "* " : ""}
+											{cat}
+										</figcaption>
 									</figure>
 								);
 							} else {
 								return (
 									<figure key={catIndex}>
 										<img src={catImport.src} alt={cat} />
-										<figcaption>{cat}</figcaption>
+										<figcaption style={{ fontStyle: isNickname ? "italic" : "normal" }}>
+											{isNickname ? "* " : ""}
+											{cat}
+										</figcaption>
 									</figure>
 								);
 							}
