9fc6973f3a73951f98f13eea37ad322a415e7777
Michael Allen Smith <digitalcolony@gmail.com>
2024-12-08 15:05:46 -0800

    v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions";
 
-import astroMetaTags from "astro-meta-tags";
-
 export default defineConfig({
-	integrations: [astroMetaTags(), react()],
+	integrations: [react()],
 });
